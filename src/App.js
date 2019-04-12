dddda29fc78927ec3c7a40abd59f286e773971e7
Juliette Michaux <miamiame@hotmail.com>
2019-04-12 10:48:37 +0200

    first merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from '../src/components/home/Home'
+
 import './App.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
+      <div>
+      <Home/>
       </div>
-    );
+      )
   }
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import ScanBut from './ScanBut';
+
+class Home extends Component {  
+  constructor() {
+    super();
+  }
+ render(){
+   return (
+     <div>
+      <ScanBut />
+     </div>
+   )
+ }
+}
+
+export default Home;
