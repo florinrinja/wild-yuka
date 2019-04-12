0b82e4dc0fa758748c80e2928e661bbcf6053603
florinrinja <florinrinja@outlook.com>
2019-04-12 10:39:28 +0200

    first merge

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Canvas from './background/Canvas'
+
+class Home extends Component {
+    
+    render(){
+        return(
+            <div>
+                <Canvas/>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/components/home/background/Canvas.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Logo from './Logo';
+
+
+
+class Canvas extends Component{
+    constructor(){
+        super();
+        this.state={
+            height:0,
+            width:0
+        };
+        window.addEventListener("resize", this.update);
+    }
+    componentDidMount(){
+        this.update();
+    }
+    update=() => {
+        this.setState({
+            height:window.innerHeight,
+            width: window.innerWidth
+        });
+    };
+    render(){
+        const viewBox = [0, 0, window.innerWidth, window.innerHeight];
+        return(
+            <svg
+            width={this.width}
+            height={this.height}
+            preserveAspectRatio="xMidYmin meet"
+            id="background"
+            viewBox={viewBox}
+            >
+            <Logo/>
+            </svg>
+        );
+    };
+}
+
+
+export default Canvas;
+
+
