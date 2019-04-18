467cc8542390136135a9670700d6504424313779
Romain-Dailly <loops51@hotmail.com>
2019-04-18 09:40:37 +0200

    indentation 2tabs

--- src/components/scan/Scan.js
+++ src/components/scan/Scan.js
@@ -11,47 +11,47 @@ export default class Scan extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			code:'',
-			productName:'',
-			productImage:'',
+			code: '',
+			productName: '',
+			productImage: '',
 			popup: false,
-			isScan:false
+			isScan: false
 		}
 	}
 
 	componentDidMount() {
-			const openPopup = () => {
-				this.setState({ popup: !this.state.popup })
-			};
-			
-			Quagga.init(
-				{
-					inputStream: {
-						type: 'LiveStream',
-						constraints: {
-							width: 640,
-							height: 480,
-							facingMode: 'environment', // or user
-						},
+		const openPopup = () => {
+			this.setState({ popup: !this.state.popup })
+		};
+
+		Quagga.init(
+			{
+				inputStream: {
+					type: 'LiveStream',
+					constraints: {
+						width: 640,
+						height: 480,
+						facingMode: 'environment', // or user
 					},
-					locator: {
-						patchSize: 'small',
-						halfSample: false,
-					},
-					numOfWorkers: 0,
-					decoder: {
-					readers: ['ean_reader'],
-						debug: {
-							drawBoundingBox: true,
-							showFrequency: false,
-							drawScanline: false,
-							showPattern: false
-						},
-						multiple: false
-					},
-					locate: true,
 				},
-				function (err) {
+				locator: {
+					patchSize: 'small',
+					halfSample: false,
+				},
+				numOfWorkers: 0,
+				decoder: {
+					readers: ['ean_reader'],
+					debug: {
+						drawBoundingBox: true,
+						showFrequency: false,
+						drawScanline: false,
+						showPattern: false
+					},
+					multiple: false
+				},
+				locate: true,
+			},
+			function (err) {
 				if (err) {
 					openPopup();
 				}
@@ -59,41 +59,41 @@ export default class Scan extends Component {
 					Quagga.start();
 				}
 			}
-			);
+		);
 		Quagga.onDetected(this._onDetected);
 		// this.getProduct()
 	}
 	// getProduct = () => {
 	// 	fetch(`https://fr.openfoodfacts.org/api/v0/produit/${this.state.code}.json`)
 	// 		.then(response  =>  response.json())
-  //     .then(response  => {
+	//     .then(response  => {
 	// 			console.log(response)
 	// 			this.setState({
 	// 				productImage:  response.product.image_front_url,
-  //         productName:		response.product.product_name,
-  //         additives:response.product.additives_original_tags
-  //       });
+	//         productName:		response.product.product_name,
+	//         additives:response.product.additives_original_tags
+	//       });
 	// 		});
 
 	// }
 	componentWillUnmount() {
 		Quagga.offDetected(this._onDetected);
 	}
-	
-	_onDetected=(data)=> {
-		this.setState({code : data.codeResult.code});
+
+	_onDetected = (data) => {
+		this.setState({ code: data.codeResult.code });
 		console.log(this.state.code)
 		// console.log(this.state.code)
-		this.setState({isScan: true})
+		this.setState({ isScan: true })
 		Quagga.pause();
 	}
-	
+
 	render() {
 		return (
-			<div style={{ Height: window.innerHeight, witdh:window.innerWidth }}>
+			<div style={{ Height: window.innerHeight, witdh: window.innerWidth }}>
 				{/* {this.state.isScan ? <div><h1>{this.state.productName}</h1><img src={this.state.productImage}></img></div>:null} */}
 				{this.state.popup ? <Link to="/" exact><PopupCam /></Link> : <div><Menu /><div id="interactive" className="viewport"></div></div>}
-			{this.state.isScan?/*<ImportData result={this.state.code}/>*/<ModalProduct />:null}
+				{this.state.isScan ?/*<ImportData result={this.state.code}/>*/ <ModalProduct /> : null}
 			</div>
 		)
 	}
