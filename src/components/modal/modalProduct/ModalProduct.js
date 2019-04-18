b2aee86c9939f19fd19e30d0c3d0b8157648fe59
rachel-os <rachel.otete@gmail.com>
2019-04-18 09:30:22 +0200

    Modal Product added

--- src/components/modal/modalProduct/ModalProduct.js
+++ src/components/modal/modalProduct/ModalProduct.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'react-materialize';
+import { Modal, Button, Table } from 'react-materialize';
 import './ModalProduct.css';
 
 const sampleProduct = {
   name : "Gerblé biscuits lait chocolat",
-  energy : "462 kcal",
+  energie : "462 kcal",
+  energiePortion : "56 kcal",
   image : "https://www.gerble.fr/var/site/storage/images/_aliases/related/vitalite/biscuit-lait-chocolat/3556-30-fre-FR/Biscuit-lait-chocolat.png",
-  graisses : "19 g",
-  glucides : "62 g",
+  graisses : "22 g",
+  graissesPortion : "2,5",
+  glucides : "58 g",
+  glucidesPortion : "6,7 g",
   fibres : "4,6 g",
-  sel : "0,75 g",
+  fibresPortion : "0,6 g",
+  proteines : "11 g",
+  proteinesPortion : "1,3",
+  sel : "0,61 g",
+  selPortion : "0,08 g"
 }
 
 class ModalProduct extends Component {
@@ -21,31 +28,78 @@ class ModalProduct extends Component {
           actions={
             <Button waves="yellow" modal="close" flat>Fermer</Button>
           }
-          header="Fiche produit" 
-          trigger={<Button 
-            id='Team'
-            floating
-            fab={{ direction: 'right'}}
-            icon="pageview"
-            className="green lighten-1"
-            style={{ left: '11%' }}
-            large
-          />}
+          header={<h4>{sampleProduct.name} </h4>} 
+          open
           >
+          <div>
+            <img src={sampleProduct.image} alt="product picture"/>  
+          </div>
           <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Nutri-score-C.svg/1920px-Nutri-score-C.svg.png' alt='Nutriscore'></img>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/NOVA_group_1.svg/800px-NOVA_group_1.svg.png' alt='Novagroup'></img>
-            <ul>
-              <li><h5>{sampleProduct.name} </h5> </li>
-              <li> <img src={sampleProduct.image} alt="product picture"/></li>
-              <li><h6>Pour 100 g</h6></li>
-              <li> Glucides : {sampleProduct.glucides} </li>
-              <li> Valeur énergétique : {sampleProduct.energy} </li>
-              <li> Matières grasses : {sampleProduct.graisses} </li>
-              <li> Fibres alimentaires : {sampleProduct.fibres} </li>
-              <li> Sel : {sampleProduct.sel} </li>
-            </ul>
           </div>
+          <Table>
+            <thead>
+              <tr>
+                <th data-field="id">
+                  Informations nutritionnelles
+                </th>
+                <th data-field="name">
+                  par portion
+                </th>
+                <th data-field="price">
+                  pour 100 g
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Énergie
+                </td>
+                <td>
+                  {sampleProduct.energiePortion}
+                </td>
+                <td>
+                  {sampleProduct.energie}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Glucides
+                </td>
+                <td>
+                  {sampleProduct.glucidesPortion} 
+                </td>
+                <td>
+                  {sampleProduct.glucides}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Lipides
+                </td>
+                <td>
+                  {sampleProduct.graissesPortion}
+                </td>
+                <td>
+                  {sampleProduct.graisses}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Protéines
+                </td>
+                <td>
+                  {sampleProduct.proteinesPortion}
+                </td>
+                <td>
+                  {sampleProduct.proteines}
+                </td>
+              </tr>
+            </tbody>
+            </Table>
+
         </Modal>
       </div>
     )
