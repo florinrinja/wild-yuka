344b30ead95227e04c4dbf25fb360a9178432354
florinrinja <florinrinja@outlook.com>
2019-06-11 14:27:16 +0200

    version 0.1

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,26 +7,20 @@ const Menu = () => {
 
   const [show, setShow] = useState(false);
 
-  const style = {
-    // backgroundColor: '#fffaf2',
-    // height: (window.innerHeight - (window.innerHeight * 0.7) - 74.98),
-  };
-
   return (
-    <div style={style}>
+    <div >
       <ButtonTeam />
       <Button id='History'
         floating
         large
         className="yellow accent-2"
-        // fab={{ direction: 'right' }}
         icon="history"
         waves="light"
         style={{ right: '3%', bottom: '2%', position: 'absolute' }}
         onClick={() => setShow(!show)}
       />
       {show ?
-        <ButtonHistory onClick={() => console.log('yum')} /> : null}
+        <ButtonHistory /> : null}
     </div>
   )
 }
